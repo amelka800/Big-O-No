cbbcfbc6ba0ffda205b4c4a9763936d559302c52
Helena Garcia <[helenagarcia946@gmail.com]>
2026-04-09 12:47:36 +0100

    "TEST"

--- pages/main.js
+++ pages/main.js
@@ -1,1 +1,23 @@
 /* This is a test */ 
+
+// Create map centered on Workday
+var map = L.map('map').setView([53.3477872,  -6.2750163], 13);
+var dest = L.map('Nail Salon').setView([53.349346, -6.279470], 16);
+
+// Add map tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Add a marker in Dublin city centre
+L.marker([53.3477872,  -6.2750163]).addTo(map)
+  .bindPopup('Dublin 7')
+  .openPopup();
+
+L.marker([53.341, -6.251])
+  .addTo(map)
+  .bindPopup("🚫 Stairs - bad accessibility");
+
+L.marker([53.346, -6.265])
+  .addTo(map)
+  .bindPopup("✅ Ramp - good accessibility");
