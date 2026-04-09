7311f460bcbdfabdd1218995f53615ea42033a9c
amelka800 <78878197+amelka800@users.noreply.github.com>
2026-04-09 13:24:21 +0100

    test

--- pages/main.js
+++ pages/main.js
@@ -1,3 +1,14 @@
 map.on('click', function(e) {
   console.log(e.latlng); // {lat, lng}
 });
+
+var score = 100;
+
+var obstructions = {
+
+  "wheelchair": {
+    "stairs": 5, 
+    "no_path": 10
+  }
+};
+
